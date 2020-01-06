05f424f8003c13dc2cd489b40063466e5128605a
Omer Atir <44023687+omeratir@users.noreply.github.com>
2020-01-06 21:42:57 +0200

    new

---
+++ AmigoIOS-master/Amigo/TabController.swift
@@ -0,0 +1,30 @@
+//
+//  TabController.swift
+//  Amigo
+//
+//  Created by Shiran Klein on 30/12/2019.
+//  Copyright © 2019 Shiran Klein. All rights reserved.
+//
+
+import UIKit
+
+class TabController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AmigoIOS-master/Amigo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Amigo
+//
+//  Created by Shiran Klein on 30/12/2019.
+//  Copyright © 2019 Shiran Klein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    @IBOutlet weak var lblsSelectedItem:UILabel?
+    @IBOutlet weak var pickerView:UIPickerView?
+    var countryName = ["Tel-Aviv", "Petah-Tiqwa","Rishon Lezion", "Tveria"]
+    let rowHieight:CGFloat = 50.0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countryName.count
+    }
+    func pickerView(_ pickerView: UIPickerView,titleForRow row:Int, ForComponent component:Int) -> String?{
+        return countryName[row]
+    }
+}
+
