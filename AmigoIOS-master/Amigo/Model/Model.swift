e5d282efb42156a1d250c4741ffc2dfe20c73587
aviadr94 <aviadr94@gmail.com>
2020-01-13 20:20:49 +0200

    Firebase

--- AmigoIOS-master/Amigo/Model/Model.swift
+++ AmigoIOS-master/Amigo/Model/Model.swift
@@ -10,20 +10,27 @@ import Foundation
 class Model {
     static let instance = Model()
     
-    
 // //   var modelSql:ModelSql = ModelSql()
-   // var modelFirebase:ModelFirebase = ModelFirebase()
-
+    var modelFirebase:ModelFirebase = ModelFirebase()
     
-    func add(student:User){
+    private init() {
+        for i in 0...0 {
+            let us = User(email:String(i),name:String(i),gender:String(i),password:String(i),country:String(i),avatar:"")
+            add(user:us)
+        }
+    }
+
+    func add(user:User){
 //        modelSql.add(student: user)l
-     //   modelFirebase.add(user: user);
+       modelFirebase.add(user: user)
+        
     }
     
-    func getAllStudents(callback:@escaping ([User]?)->Void){
+    func getAllUsers()->[User]{
      //   modelFirebase.getAllUsers(callback: callback);
 // //       return modelSql.getAllStudents()
 //        modelFirebase.getAllStudents();
+        modelFirebase.getAllUsers()
     }
     
 }
--- AmigoIOS-master/Amigo/Model/ModelFirebase.swift
+++ AmigoIOS-master/Amigo/Model/ModelFirebase.swift
@@ -7,38 +7,46 @@
 //
 
 import Foundation
-
 import Firebase
 
 class ModelFirebase{
     
     func add(user:User){
-//        let db = Firestore.firestore()
-//        // Add a new document with a generated ID
-//        var ref: DocumentReference? = nil
-//        ref = db.collection("users").addDocument(data: user.toJson(), completion: { err in
-//                if let err = err {
-//                    print("Error adding document: \(err)")
-//                } else {
-//                    print("Document added with ID: \(ref!.documentID)")
-//                }
-//        })
+       let db = Firestore.firestore()
+        // Add a new document with a generated ID
+        var ref: DocumentReference? = nil
+        ref = db.collection("users").addDocument(data: [
+            "first": "Ada",
+            "last": "Lovelace",
+            "last": "Lovelace",
+            "last": "Lovelace",
+            "born": "Lovelace",
+            "born": "Lovelace"
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+
     }
     
-    func getAllUsers(callback: @escaping ([User]?)->Void){
-        let db = Firestore.firestore()
-        db.collection("users").getDocuments { (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
-                callback(nil);
-            } else {
-                var data = [User]();
-                for document in querySnapshot!.documents {
-                    data.append(User(json: document.data()));
-                }
-                callback(data);
-            }
-        };
+    func getAllUsers()->[User] {
+        return [User]()
+//        let db = Firestore.firestore()
+//        db.collection("users").getDocuments { (querySnapshot, err) in
+//            if let err = err {
+//                print("Error getting documents: \(err)")
+//                callback(nil);
+//            } else {
+//                var data = [User]();
+//                for document in querySnapshot!.documents {
+//                    data.append(User(json: document.data()));
+//                }
+//                callback(data);
+//            }
+//        };
     }
 }
 
