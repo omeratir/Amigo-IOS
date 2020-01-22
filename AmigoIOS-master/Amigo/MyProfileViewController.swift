fcbcb71c45ce703321f9ef5bd8c73b1ce98a3a5e
aviadr94 <aviadr94@gmail.com>
2020-01-22 18:26:51 +0200

    shiran-after we talk with eliav we restart the registar.

--- AmigoIOS-master/Amigo/MyProfileViewController.swift
+++ AmigoIOS-master/Amigo/MyProfileViewController.swift
@@ -12,15 +12,51 @@ import Firebase
 import FirebaseStorage
 class MyProfileViewController: UIViewController {
 
-    
     @IBOutlet weak var ImageProfile: UIImageView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //let store = Storage.storage()
+   let storage = Storage.storage()
+        let storageRef = storage.reference()
+        let gsReference = storage.reference(forURL: "gs://amigo-e1b90.appspot.com/email")
+        let user = Auth.auth().currentUser
+        let islandRef = storageRef.child("\(user?.email)")
+        
+        islandRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+          if let error = error {
+            // Uh-oh, an error occurred!
+          } else {
+            // Data for "images/island.jpg" is returned
+            self.ImageProfile.image = UIImage(data: data!)
+          }
+        }
+           
+        // Create a reference from a Google Cloud Storage URI
+//        let gsReference = store.reference(forURL: "gs://amigo-e1b90.appspot.com/email")
+//
+//       let user = Auth.auth().currentUser
+//      //  let userProfilesRef = storeRef.child("email")
+//       //  let pathReference = store.reference(withPath: "email")
+//       // let islandRef = gsReference.child("\(user?.email)")
+//        print ("\(user?.email)")
+//        //userProfilesRef.child("\(email).png")
+//        // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
+//        gsReference.getData(maxSize: 1 * 1024 * 1024) { data, error in
+//          if let error = error {
+//            print ("Uh-oh")
+//            // Uh-oh, an error occurred!
+//          } else {
+//            // Data for "images/island.jpg" is returned
+//            self.ImageProfile.image = UIImage(data: data!)
+//            print("yay")
+//
+//          }
+//        }
+            
     }
+
+
     //url!.absoluteString
  
 
--- AmigoIOS-master/Amigo/RegisterViewController.swift
+++ AmigoIOS-master/Amigo/RegisterViewController.swift
@@ -18,10 +18,10 @@ protocol RegiserDelegate{
 
 class RegisterViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-   //  var delegate:RegiserDelegate?
+    //  var delegate:RegiserDelegate?
     
     var imagePicker = UIImagePickerController()
-
+    
     @IBOutlet weak var imageViewAvatar: UIImageView!
     
     @IBOutlet weak var pickerCity: UIPickerView!
@@ -33,64 +33,81 @@ class RegisterViewController: UIViewController, UIPickerViewDataSource,UIPickerV
     
     
     func onComplete(success: Bool) {
-         print("on Complete signInOut \(success)")
-                   if success == true {
-                       print("on Complete signInOut success")
-                       let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                       let signInVC = storyboard.instantiateViewController(withIdentifier: "TabController")
-                       self.present(signInVC, animated: true, completion: nil)
-                   }
-       }
+        print("on Complete signInOut \(success)")
+        if success == true {
+            print("on Complete signInOut success")
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let signInVC = storyboard.instantiateViewController(withIdentifier: "TabController")
+            self.present(signInVC, animated: true, completion: nil)
+        }
+    }
     
     @IBAction func finishRegister(_ sender: Any) {
-          print("sign up pressed")
-            //dismiss keyboard
-            self.view.endEditing(true)
-            ProgressHUD.show("Waiting...",interaction: false)
-            //case fields are empty
-            if(emailUser.text?.isEmpty ?? true || passwordUser.text?.isEmpty ?? true){
+        print("sign up pressed")
+        //dismiss keyboard
+        self.view.endEditing(true)
+        ProgressHUD.show("Waiting...",interaction: false)
+        //case fields are empty
+        if(emailUser.text?.isEmpty ?? true || passwordUser.text?.isEmpty ?? true){
             
-                // alert message "fill all fields"
-                ProgressHUD.showError("PLEASE fill all fields")
-                return
-            }
-            
-            //values for sign
-            guard let email = emailUser.text else {return}
-            guard let password = passwordUser.text else {return}
-            guard let username = fullName.text else {return}
-            //set profile image
-            guard let profileImg = self.imageViewAvatar.image else {return}
-            //upload data
-            guard let uploadData : Data = profileImg.jpegData(compressionQuality: 0.3) else {return}
-         let db = Firestore.firestore()
+            // alert message "fill all fields"
+            ProgressHUD.showError("PLEASE fill all fields")
+            return
+        }
+        
+        //values for sign
+        guard let email = emailUser.text else {return}
+        guard let password = passwordUser.text else {return}
+        guard let username = fullName.text else {return}
+        //set profile image
+        guard let profileImg = self.imageViewAvatar.image else {return}
+        //upload data
+        guard let uploadData : Data = profileImg.jpegData(compressionQuality: 0.3) else {return}
+        let db = Firestore.firestore()
+        //db.collection("users").document(<#T##documentPath: String##String#>)
         db.collection("users").addDocument(data: ["fullname": username,"email":email,"password":password])
-        AuthUsers.signUp(username: username, email: email, password: password, imageData: uploadData, onSuccess: {
-                
-                print("Seccessfuly created user and saved information to DB")
-                
-                ProgressHUD.showSuccess("Success")
-                print("Successfully signed user up" )
-             //   self.dismiss(animated: true, completion: {() in
-                self.onComplete(success: true);
-            //    })
-                
-            }) { (errorMsg) in
-                ProgressHUD.showError(errorMsg)
+        
+        let store = Storage.storage()
+        let storeRef = store.reference()
+        let userProfilesRef = storeRef.child("email")
+        //let logoRef = storeRef.child("images.png")
+        // let userProfiles = logoRef.parent()?.child("profiles")
+        guard let ProfileImage = self.imageViewAvatar.image else {return}
+        //upload data
+        guard let UpTheData : Data = profileImg.jpegData(compressionQuality: 0.3) else {return}
+        let uploadUserProfileTask = userProfilesRef.child("\(email)").putData(UpTheData, metadata: nil) { (metadata, error) in
+            guard let metadata = metadata else {
+                print("Error occurred: \(error)")
                 return
             }
         }
+        print ("\(email)")
+        AuthUsers.signUp(username: username, email: email, password: password, imageData: uploadData, onSuccess: {
+            
+            print("Seccessfuly created user and saved information to DB")
+            
+            ProgressHUD.showSuccess("Success")
+            print("Successfully signed user up" )
+            //   self.dismiss(animated: true, completion: {() in
+            self.onComplete(success: true);
+            //    })
+            
+        }) { (errorMsg) in
+            ProgressHUD.showError(errorMsg)
+            return
+        }
+    }
     
-//    @IBAction func loginConnect(_ sender: Any) {
-//        Auth.auth().signIn(withEmail: self.emailUser.text! , password: self.passwordUser.text!) { (user,error) in
-//                   if user != nil {
-//                   print("aviad")
-//                   }
-//                   if error != nil {
-//                       print(":(")
-//                   }
-//        }
-//    }
+    //    @IBAction func loginConnect(_ sender: Any) {
+    //        Auth.auth().signIn(withEmail: self.emailUser.text! , password: self.passwordUser.text!) { (user,error) in
+    //                   if user != nil {
+    //                   print("aviad")
+    //                   }
+    //                   if error != nil {
+    //                       print(":(")
+    //                   }
+    //        }
+    //    }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -100,8 +117,8 @@ class RegisterViewController: UIViewController, UIPickerViewDataSource,UIPickerV
         return pickerData.count
     }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-         return pickerData[row]
-     }
+        return pickerData[row]
+    }
     
     //Upload photo button
     @IBAction func uploadPhotoBtn(_ sender: Any)
@@ -110,7 +127,7 @@ class RegisterViewController: UIViewController, UIPickerViewDataSource,UIPickerV
         imagePickerController.delegate = self
         
         let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
-
+        
         actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in
             
             if(UIImagePickerController.isSourceTypeAvailable(.camera))
@@ -122,7 +139,7 @@ class RegisterViewController: UIViewController, UIPickerViewDataSource,UIPickerV
             {
                 print("Camera not available")
             }
-
+            
         }))
         
         
@@ -160,7 +177,7 @@ class RegisterViewController: UIViewController, UIPickerViewDataSource,UIPickerV
         let layer = CAGradientLayer()
         let color1 = UIColor(red:0.99, green: 0.48, blue: 0.48, alpha: 1.0)
         let color2 = UIColor(red:0.65, green: 0.76, blue: 1.00, alpha: 1.0)
-
+        
         layer.colors = [ color1.cgColor ,color2.cgColor]
         layer.frame = view.frame
         view.layer.insertSublayer(layer, at: 0)
@@ -172,7 +189,7 @@ class RegisterViewController: UIViewController, UIPickerViewDataSource,UIPickerV
         self.pickerCity.dataSource = self
         pickerData = ["Tel-Aviv", "Petah-Tiqwa","Rishon Lezion", "Tveria"]
     }
-
+    
 }
 extension RegisterViewController : UITextFieldDelegate{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
